b5b1d66417ef39d5e6adc8242552d9f80362c120
aswami98 <aswami@purdue.edu>
2018-04-24 14:32:47 -0400

    only using first 50% of training training and removing stop words

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ trainNumRows = trainingData.shape[0]
 trainNumColumns = trainingData.shape[1]
 #print(trainNumRows)
 #print(trainNumColumns)
+ogX = trainingData
+size = int(trainNumRows*(50.0/100))
 
 #data = pd.read_csv(testData, sep=',', error_bad_lines=False, header=None)
 data = pd.read_csv(testData, sep=',', quotechar='"', header=0)
@@ -28,6 +30,11 @@ one = {} # Negative
 two = {} # Neutral
 three = {} # Positive
 four = {} # Very Positive
+redundantFeatures = ["the", "a", "an", "of", "to", "in", "its", "on", "is", "by", "from", "it", "and", "with", "for", "off"]
+
+trainingData = ogX[:size]
+trainNumRows = trainingData.shape[0]
+trainNumColumns = trainingData.shape[1]
 
 for id, text, sentiment in trainingData:
     text = text.replace(';', '')
@@ -35,6 +42,9 @@ for id, text, sentiment in trainingData:
     text = text.replace('\'', '')
 
     for word in text.split():
+        #word = word.lower()
+        if word in redundantFeatures:
+            continue
         if sentiment == 0:
             zero[word] = zero.get(word, 0) + 1
         elif sentiment == 1:
@@ -46,7 +56,6 @@ for id, text, sentiment in trainingData:
         elif sentiment == 4:
             four[word] = four.get(word, 0) + 1
 
-redundantFeatures = ["the", "a", "an", "of", "to", "in", "its", "on", "is", "by", "from", "it", "and", "with", "for", "off"]
 result = {}
 #for i in range(testNumRows):
 #    result.append(0)
@@ -62,7 +71,7 @@ for id, text in testData:
     text = text.replace('\'', '')
 
     for word in text.split():
-        word = word.lower()
+        #word = word.lower()
         if word in redundantFeatures:
             continue
         countZero = zero.get(word, 0) + 1.0
@@ -91,3 +100,40 @@ with solution:
    writer.writerow(["id", "sentiment"])
    for key, value in result.items():
        writer.writerow([key, value])
+
+trainingData = ogX[:-size]
+trainNumRows = trainingData.shape[0]
+trainNumColumns = trainingData.shape[1]
+
+correct = 0.0
+for id, text, sentiment in trainingData:
+    if id == "id":
+        continue
+
+    probZero, probOne, probTwo, probThree, probFour = 0.0, 0.0, 0.0, 0.0, 0.0
+
+    text = text.replace(';', '')
+    text = text.replace(',', '')
+    text = text.replace('\'', '')
+
+    for word in text.split():
+        #word = word.lower()
+        if word in redundantFeatures:
+            continue
+        countZero = zero.get(word, 0) + 1.0
+        countOne = one.get(word, 0) + 1.0
+        countTwo = two.get(word, 0) + 1.0
+        countThree = three.get(word, 0) + 1.0
+        countFour = four.get(word, 0) + 1.0
+
+        probZero += countZero / (countZero + countOne + countTwo + countThree + countFour)
+        probOne += countOne / (countZero + countOne + countTwo + countThree + countFour)
+        probTwo += countTwo / (countZero + countOne + countTwo + countThree + countFour)
+        probThree += countThree / (countZero + countOne + countTwo + countThree + countFour)
+        probFour += countFour / (countZero + countOne + countTwo + countThree + countFour)
+
+    prediction = np.array([probZero, probOne, probTwo, probThree, probFour]).argmax(axis = 0)
+    if prediction == sentiment:
+        correct += 1
+
+print(correct/trainNumRows)
